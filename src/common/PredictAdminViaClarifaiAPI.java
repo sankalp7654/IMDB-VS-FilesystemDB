248bac9bcc6e5ddcb42ecd975e33e75450e49093
sankalp7654 <sankalp.saxena.sta@gmail.com>
2018-03-04 22:41:32 +0530

    added gui with search tabbed pan

--- src/common/PredictAdminViaClarifaiAPI.java
+++ src/common/PredictAdminViaClarifaiAPI.java
@@ -2,6 +2,8 @@ package common;
 import java.io.File;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import clarifai2.api.ClarifaiBuilder;
 import clarifai2.api.ClarifaiClient;
 import clarifai2.api.request.model.PredictRequest;
@@ -13,39 +15,43 @@ public class PredictAdminViaClarifaiAPI {
 	
 	public double getResponse(String path) {
 		
-		ClarifaiClient client = new ClarifaiBuilder("f3bd121873dd4162847aa6bbb74e7599").buildSync();
+		try {
+			ClarifaiClient client = new ClarifaiBuilder("f3bd121873dd4162847aa6bbb74e7599").buildSync();
 
-		// Predicts the model
-		PredictRequest<Prediction> request = client.predict("adminPrediction")
-			    .withInputs(ClarifaiInput.forImage(new File(path)));
-		
-		// Stores the response
-		List <ClarifaiOutput<Prediction>> result = request.executeSync().get();
-		
-		String resultString = result.toString();
-		
-		// getting the predicted value of Admin - Sankalp Saxena   
-		int startIndex = resultString.indexOf("Concept{id=sankalp saxena, name=sankalp saxena,", 0);
-		startIndex = resultString.indexOf(" value=", startIndex);
-		
-		// value=0.21268915 need to extract this value 
-		startIndex = startIndex + 7;
-		
-		char getValue [] = new char[20];
-		int j=0;
+			// Predicts the model
+			PredictRequest<Prediction> request = client.predict("adminPrediction")
+				    .withInputs(ClarifaiInput.forImage(new File(path)));
+			
+			// Stores the response
+			List <ClarifaiOutput<Prediction>> result = request.executeSync().get();
+			
+			String resultString = result.toString();
+			
+			// getting the predicted value of Admin - Sankalp Saxena   
+			int startIndex = resultString.indexOf("Concept{id=sankalp saxena, name=sankalp saxena,", 0);
+			startIndex = resultString.indexOf(" value=", startIndex);
+			
+			// value=0.21268915 need to extract this value 
+			startIndex = startIndex + 7;
+			
+			char getValue [] = new char[20];
+			int j=0;
 
-		for(int i=startIndex; i<=startIndex+9; i++) {
-			getValue[j++] = resultString.charAt(i);
+			for(int i=startIndex; i<=startIndex+9; i++) {
+				getValue[j++] = resultString.charAt(i);
+			}
+			
+			StringToInteger obj1 = new StringToInteger();
+			Double value = obj1.parseInt(getValue);
+			
+			return value;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(null, "API Call was not Successful!", "Error", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
 		}
-		
-		StringToInteger obj1 = new StringToInteger();
-        Double value = obj1.parseInt(getValue);
-        
-        return value;
-        //  System.out.println(value);
-		
-	//	System.out.println(result);
-	   
+		return 0L;
 	}
+
 }	
 
--- src/views/IMDB_GUI.java
+++ src/views/IMDB_GUI.java
@@ -23,6 +23,7 @@ import common.FileSystemDB;
 import common.InMemoryDB;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.border.EtchedBorder;
 
 public class IMDB_GUI extends JFrame {
 
@@ -40,6 +41,15 @@ public class IMDB_GUI extends JFrame {
 	private JPanel panel_2;
 	private JTextField txt_filesystem_time;
 	private JTextField txt_imdb_time;
+	private JTextField txt_search_user_id;
+	private JTextField txt_search_filesystem_time;
+	private JTextField txt_search_imdb_time;
+	private JTextField txt_search_username;
+	private JTextField txt_search_first_name;
+	private JTextField txt_search_last_name;
+	private JTextField txt_search_gender;
+	private JTextField txt_search_password;
+	private JTextField txt_search_status;
 
 	/**
 	 * Create the frame.
@@ -137,8 +147,8 @@ public class IMDB_GUI extends JFrame {
 		txt_status = new JTextField();
 		txt_status.setColumns(10);
 		
-		JButton button = new JButton("INSERT INTO IMDB");
-		button.addActionListener(new ActionListener() {
+		JButton btn_insert_into_imdb = new JButton("INSERT INTO IMDB");
+		btn_insert_into_imdb.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String userDetails[] = new String[5];
 				int userId = Integer.parseInt(txt_user_id.getText());
@@ -164,8 +174,8 @@ public class IMDB_GUI extends JFrame {
 			}
 		});
 		
-		JButton button_1 = new JButton("INSERT INTO FILESYSTEM");
-		button_1.addActionListener(new ActionListener() {
+		JButton btn_insert_into_filesystem = new JButton("INSERT INTO FILESYSTEM");
+		btn_insert_into_filesystem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String userDetails[] = new String[7];
 				userDetails[0] = txt_user_id.getText().toString();
@@ -231,9 +241,9 @@ public class IMDB_GUI extends JFrame {
 					.addComponent(txt_status, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addGap(43)
-					.addComponent(button, GroupLayout.PREFERRED_SIZE, 168, GroupLayout.PREFERRED_SIZE)
+					.addComponent(btn_insert_into_imdb, GroupLayout.PREFERRED_SIZE, 168, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
-					.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE))
+					.addComponent(btn_insert_into_filesystem, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE))
 		);
 		gl_panel_2.setVerticalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
@@ -283,8 +293,8 @@ public class IMDB_GUI extends JFrame {
 						.addComponent(txt_status, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
-						.addComponent(button, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)))
+						.addComponent(btn_insert_into_imdb, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btn_insert_into_filesystem, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)))
 		);
 		panel_2.setLayout(gl_panel_2);
 		
@@ -298,8 +308,8 @@ public class IMDB_GUI extends JFrame {
 		JLabel lbl_filesystem_time = new JLabel("File System:");
 		lbl_filesystem_time.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
 		
-		JButton button_2 = new JButton("Clear");
-		button_2.addActionListener(new ActionListener() {
+		JButton btn_clear = new JButton("Clear");
+		btn_clear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				txt_user_id.setText("");
 				txt_user_name.setText("");
@@ -313,8 +323,8 @@ public class IMDB_GUI extends JFrame {
 			}
 		});
 		
-		JButton button_3 = new JButton("Exit");
-		button_3.addActionListener(new ActionListener() {
+		JButton btn_exit = new JButton("Exit");
+		btn_exit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
@@ -339,9 +349,9 @@ public class IMDB_GUI extends JFrame {
 					.addPreferredGap(ComponentPlacement.RELATED, 87, Short.MAX_VALUE)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING, false)
 						.addGroup(gl_panel_1.createSequentialGroup()
-							.addComponent(button_2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btn_clear, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(button_3))
+							.addComponent(btn_exit))
 						.addComponent(txt_filesystem_time, Alignment.LEADING)
 						.addComponent(txt_imdb_time, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE))
 					.addGap(38))
@@ -360,8 +370,8 @@ public class IMDB_GUI extends JFrame {
 						.addComponent(txt_filesystem_time, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
-						.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btn_exit, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btn_clear, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap(453, Short.MAX_VALUE))
 		);
 		panel_1.setLayout(gl_panel_1);
@@ -387,7 +397,258 @@ public class IMDB_GUI extends JFrame {
 		panel.setLayout(gl_panel);
 		
 		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(248, 248, 255));
 		tabbedPane.addTab("SEARCH", null, panel_3, null);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setSize(new Dimension(300, 100));
+		panel_4.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "SEARCH A RECORD", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_4.setBackground(new Color(255, 239, 213));
+		
+		JLabel lbl_search_user_id = new JLabel("User ID:");
+		lbl_search_user_id.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		txt_search_user_id = new JTextField();
+		txt_search_user_id.setColumns(10);
+		
+		JButton btn_search_insert_in_imdb = new JButton("SEARCH IN IMDB");
+		
+		JButton btn_search_in_filesystemdb = new JButton("SEARCH IN FILESYSTEM");
+		GroupLayout gl_panel_4 = new GroupLayout(panel_4);
+		gl_panel_4.setHorizontalGroup(
+			gl_panel_4.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_4.createSequentialGroup()
+					.addGroup(gl_panel_4.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_4.createSequentialGroup()
+							.addGap(32)
+							.addGroup(gl_panel_4.createParallelGroup(Alignment.TRAILING)
+								.addComponent(btn_search_in_filesystemdb)
+								.addGroup(gl_panel_4.createSequentialGroup()
+									.addComponent(lbl_search_user_id, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+									.addGap(12)
+									.addComponent(txt_search_user_id, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))))
+						.addGroup(gl_panel_4.createSequentialGroup()
+							.addGap(28)
+							.addComponent(btn_search_insert_in_imdb, GroupLayout.PREFERRED_SIZE, 172, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(36, Short.MAX_VALUE))
+		);
+		gl_panel_4.setVerticalGroup(
+			gl_panel_4.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_4.createSequentialGroup()
+					.addGap(15)
+					.addGroup(gl_panel_4.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_4.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lbl_search_user_id))
+						.addComponent(txt_search_user_id, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel_4.createParallelGroup(Alignment.LEADING)
+						.addComponent(btn_search_insert_in_imdb, GroupLayout.DEFAULT_SIZE, 36, Short.MAX_VALUE)
+						.addComponent(btn_search_in_filesystemdb, GroupLayout.DEFAULT_SIZE, 36, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		panel_4.setLayout(gl_panel_4);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new TitledBorder(null, "TIME ELAPSED", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_5.setBackground(new Color(204, 255, 204));
+		
+		JLabel lbl_search_imdb_time = new JLabel("In Memory DataBase:");
+		lbl_search_imdb_time.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		JLabel lbl_search_filesystem_time = new JLabel("File System:");
+		lbl_search_filesystem_time.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		JButton btn_search_clear = new JButton("Clear");
+		
+		JButton btn_search_exit = new JButton("Exit");
+		
+		txt_search_filesystem_time = new JTextField();
+		txt_search_filesystem_time.setEditable(false);
+		txt_search_filesystem_time.setColumns(10);
+		
+		txt_search_imdb_time = new JTextField();
+		txt_search_imdb_time.setEditable(false);
+		txt_search_imdb_time.setColumns(10);
+		GroupLayout gl_panel_5 = new GroupLayout(panel_5);
+		gl_panel_5.setHorizontalGroup(
+			gl_panel_5.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_5.createSequentialGroup()
+					.addGap(33)
+					.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING)
+						.addComponent(lbl_search_imdb_time)
+						.addComponent(lbl_search_filesystem_time, GroupLayout.PREFERRED_SIZE, 166, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 68, Short.MAX_VALUE)
+					.addGroup(gl_panel_5.createParallelGroup(Alignment.TRAILING, false)
+						.addGroup(gl_panel_5.createSequentialGroup()
+							.addComponent(btn_search_clear, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btn_search_exit))
+						.addComponent(txt_search_filesystem_time, Alignment.LEADING)
+						.addComponent(txt_search_imdb_time, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE))
+					.addGap(33))
+		);
+		gl_panel_5.setVerticalGroup(
+			gl_panel_5.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel_5.createSequentialGroup()
+					.addContainerGap(240, Short.MAX_VALUE)
+					.addGroup(gl_panel_5.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbl_search_imdb_time)
+						.addComponent(txt_search_imdb_time, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel_5.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbl_search_filesystem_time)
+						.addComponent(txt_search_filesystem_time, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel_5.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btn_search_exit, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btn_search_clear, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE))
+					.addGap(18))
+		);
+		panel_5.setLayout(gl_panel_5);
+		
+		JPanel pnl_record_details = new JPanel();
+		pnl_record_details.setBackground(new Color(230, 230, 250));
+		pnl_record_details.setBorder(new TitledBorder(null, "RECORD DETAILS", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		GroupLayout gl_panel_3 = new GroupLayout(panel_3);
+		gl_panel_3.setHorizontalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel_3.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_3.createParallelGroup(Alignment.TRAILING)
+						.addComponent(pnl_record_details, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 470, Short.MAX_VALUE)
+						.addComponent(panel_4, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 470, Short.MAX_VALUE)
+						.addComponent(panel_5, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 470, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_panel_3.setVerticalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_3.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(pnl_record_details, GroupLayout.DEFAULT_SIZE, 271, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		
+		JLabel lbl_search_username = new JLabel("User Name:");
+		lbl_search_username.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		txt_search_username = new JTextField();
+		txt_search_username.setEditable(false);
+		txt_search_username.setColumns(10);
+		
+		JLabel lbl_search_first_name = new JLabel("First Name:");
+		lbl_search_first_name.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		txt_search_first_name = new JTextField();
+		txt_search_first_name.setEditable(false);
+		txt_search_first_name.setColumns(10);
+		
+		txt_search_last_name = new JTextField();
+		txt_search_last_name.setEditable(false);
+		txt_search_last_name.setColumns(10);
+		
+		JLabel lbl_search_last_name = new JLabel("Last Name:");
+		lbl_search_last_name.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		JLabel lbl_search_gender = new JLabel("Gender:");
+		lbl_search_gender.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		txt_search_gender = new JTextField();
+		txt_search_gender.setEditable(false);
+		txt_search_gender.setColumns(10);
+		
+		txt_search_password = new JTextField();
+		txt_search_password.setEditable(false);
+		txt_search_password.setColumns(10);
+		
+		JLabel lbl_search_password = new JLabel("Password:");
+		lbl_search_password.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		JLabel lbl_search_status = new JLabel("Status:");
+		lbl_search_status.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		txt_search_status = new JTextField();
+		txt_search_status.setEditable(false);
+		txt_search_status.setColumns(10);
+		GroupLayout gl_pnl_record_details = new GroupLayout(pnl_record_details);
+		gl_pnl_record_details.setHorizontalGroup(
+			gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnl_record_details.createSequentialGroup()
+					.addGap(31)
+					.addGroup(gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addComponent(lbl_search_username, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txt_search_username, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addComponent(lbl_search_first_name, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txt_search_first_name, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addComponent(lbl_search_last_name, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+							.addGap(16)
+							.addComponent(txt_search_last_name, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addComponent(lbl_search_gender, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+							.addGap(36)
+							.addComponent(txt_search_gender, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addComponent(lbl_search_password, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
+							.addGap(21)
+							.addComponent(txt_search_password, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addComponent(lbl_search_status, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+							.addGap(44)
+							.addComponent(txt_search_status, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(37, Short.MAX_VALUE))
+		);
+		gl_pnl_record_details.setVerticalGroup(
+			gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnl_record_details.createSequentialGroup()
+					.addGap(15)
+					.addGroup(gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lbl_search_username, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txt_search_username, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(12)
+					.addGroup(gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lbl_search_first_name, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txt_search_first_name, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(12)
+					.addGroup(gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lbl_search_last_name, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txt_search_last_name, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(12)
+					.addGroup(gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lbl_search_gender, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txt_search_gender, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(12)
+					.addGroup(gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lbl_search_password, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txt_search_password, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(12)
+					.addGroup(gl_pnl_record_details.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnl_record_details.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lbl_search_status, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txt_search_status, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(16, Short.MAX_VALUE))
+		);
+		pnl_record_details.setLayout(gl_pnl_record_details);
+		panel_3.setLayout(gl_panel_3);
 		getContentPane().setLayout(groupLayout);
 		
 		
